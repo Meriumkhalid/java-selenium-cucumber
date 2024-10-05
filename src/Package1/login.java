81fb627679d7b909788905c5a2caa4e274855ec8
meriumkhalid <mariamkhalid0346@gmail.com>
2024-10-05 17:17:51 +0500

    first commit

---
+++ src/Package1/login.java
@@ -0,0 +1,14 @@
+package Package1;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3 , b=5;
+		int add;
+		add=a+b;
+		System.out.println(add);
+
+	}
+
+}
